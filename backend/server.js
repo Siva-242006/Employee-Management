224a748293ed585c97d4db7a829af3c8b573b381
Siva-242006 <sivas393922@gmail.com>
2025-06-28 22:07:52 +0530

    update

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
@@ -6,6 +7,9 @@ const employeeRoutes = require('./routes/employees');
 const companyRoutes = require('./routes/companies');
 
 const app = express();
+
+const PORT = process.env.PORT || 5000;
+const MONGO_URI = process.env.MONGO_URI;
 
 app.use(cors());
 app.use(express.json()); 
